06295d2b4fb30015878ae3891fffa0f3e96ab441
Piush Gupta <piush.gupta@rsystems.com>
2014-02-21 12:09:32 +0530

    new fiscal stats table added

---
+++ db/migrate/20140221055750_create_fiscal_stats.rb
@@ -0,0 +1,17 @@
+class CreateFiscalStats < ActiveRecord::Migration
+  def change
+    create_table :fiscal_stats do |t|
+    	t.integer :fiscal_year, limit: 4
+    	t.decimal :opening_blnc, :precision => 15, :scale => 2
+    	t.decimal :closing_blnc, :precision => 15, :scale => 2
+    	t.string :currency_type
+    	t.decimal :total_revenue, :precision => 15, :scale => 2
+    	t.decimal :net_profit, :precision => 15, :scale => 2
+    	t.decimal :available_social_fund, :precision => 15, :scale => 2
+    	t.integer :user_id
+    	t.boolean :eligibility_status
+
+      t.timestamps
+    end
+  end
+end

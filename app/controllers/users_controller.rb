2192ddbdf13ca33c5b5eb4619130e1271ff55599
Piush Gupta <piush.gupta@rsystems.com>
2014-02-24 19:48:57 +0530

    role wise pages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,5 +4,16 @@ class UsersController < ApplicationController
 	end
 
 	def dashboard
+		@user = current_user
+		if current_user.role == "Corporate"
+			render "users/as_corporate"
+			return
+		elsif current_user.role == "NGO"
+			render "users/as_ngo.html.erb"
+			return
+		elsif current_user.role == "Socialist"
+			render "users/as_socialist"
+			return
+		end
 	end
 end

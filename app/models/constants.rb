0e3f7cff08b6aa115a0b39165909b256f15c15a1
Piush Gupta <piush.gupta@rsystems.com>
2014-03-04 12:04:58 +0530

    fiscal year drop down

--- app/models/constants.rb
+++ app/models/constants.rb
@@ -4,4 +4,17 @@ module Constants
 
 	# percentage value the should be deducted from the net profit as social_fund
 	SOCIALFUND_RATIO = 0.02
+
+	#list of last 10 financial_years
+	yr_range = []
+	two_yr_bck = Time.now.year - 2
+	((two_yr_bck - 9)..two_yr_bck).each do |yr|
+		yr_range << (yr.to_s+'-'+(yr+1).to_s)
+	end
+
+	FISCAL_YEAR = yr_range
+
+	# valid currencies list
+	VALID_CURRENCIES = ["INR"]
+
 end

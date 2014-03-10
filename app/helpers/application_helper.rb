0e0257a32ed61defc17d7422d881d02dacd4b0dc
Piush Gupta <piush.gupta@rsystems.com>
2014-03-10 14:07:23 +0530

    see proposals

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -49,13 +49,18 @@ module ApplicationHelper
 		opt = ""
 		if iss.user_id == current_user.id
 			if iss.issue_status == Constants::IssueStatusConstant.all_to_hash[:open]
-				opt << '<a href="<%= edit_issue_path(id: iss.id) %>" class="btn btn-xs btn-danger"  data-rel="tooltip" title="" data-original-title="Edit Project"><i class="icon-edit bigger-120"></i></a>'
+				opt << "<a href=#{edit_issue_path(id: iss.id)} class='btn btn-xs btn-danger'  data-rel='tooltip' title='' data-original-title='Edit Project'><i class='icon-edit bigger-120'></i></a>"
 			end
 		end
 
 		if current_user.role == "NGO"
 			opt << '<a id="int_btn_<%= iss.id%>" onClick="return sendProposal(<%= iss.id %>);" class="btn btn-xs btn-info" data-rel="tooltip" title="" data-original-title="Send Proposal"><i class="icon-download-alt bigger-120"></i></a>'
 		end
+
+		if current_user.role == "Analyst"
+			opt << "<a href=#{proposals_path(issue_id: iss.id)} class='btn btn-xs btn-info' data-rel='tooltip' title='' data-original-title='See Proposals'><i class='icon-download-alt bigger-120'></i></a>"
+		end
+
 		opt
 	end
 

2418f91c3c301cd76997d48f7e99ff0d7223e07f
Piush Gupta <piush.gupta@rsystems.com>
2014-03-12 13:34:41 +0530

    related to money

--- app/models/project.rb
+++ app/models/project.rb
@@ -3,17 +3,4 @@ class Project < ActiveRecord::Base
 	belongs_to :user
 	has_many :project_reports
 
-	#after_create :update_kitty_amount
-
-	private
-
-	# def update_kitty_amount
-	# 	project_cost = self.project_actual_cost
-	# 	scl_fund = SocialFund.order("fiscal_year DESC").first
-	# 	if scl_fund.present?
-	# 		scl_fund.fund_used += project_cost
-	# 		scl_fund.fund_remains -= project_cost
-	# 		scl_fund.save
-	# 	end
-	# end
 end
